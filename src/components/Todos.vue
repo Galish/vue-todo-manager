7d51ba250e456985c3946eaac454ac4ed707f69c
Galish <galish.ufa@gmail.com>
2020-07-10 10:21:30 +0500

    Add/remove/delete todos

---
+++ src/components/Todos.vue
@@ -0,0 +1,62 @@
+<template>
+	<div>
+		<h3>Todos</h3>
+
+		<div class="todos">
+			<div
+				class="todo"
+				v-for="todo in allTodos"
+				:key="todo.id"
+			>
+				{{todo.title}}
+
+				<i
+					class="delete"
+					@click="deleteTodo(todo.id)"
+				>
+					x
+				</i>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	import { mapActions, mapGetters } from 'vuex'
+
+	export default {
+		name: 'Todos',
+		computed: mapGetters([ 'allTodos' ]),
+		methods: {
+			...mapActions([ 'fetchTodos', 'deleteTodo' ])
+		},
+		created() {
+			this.fetchTodos()
+		}
+	}
+</script>
+
+<style scoped>
+	.todos {
+		display: grid;
+		grid-template-columns: repeat(3, 1fr);
+		grid-gap: 1rem;
+	}
+
+	.todo {
+		border: 1px solid #ccc;
+		background: #41b883;
+		padding: 1rem;
+		border-radius: 5px;
+		text-align: center;
+		position: relative;
+		cursor: pointer;
+	}
+
+	.delete {
+		font-style: normal;
+		position: absolute;
+		right: 10px;
+		bottom: 5px;
+	}
+</style>
